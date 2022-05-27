157b54e57162ff38ae1a944d2efd18dd08eac2dd
ujin-Bang <qkddnwls14@gmail.com>
2022-05-27 11:42:28 +0900

    프래그먼트 동작 코드  - 화면 전달 : requireContext() 활용  - 프래그먼트는 화면이 아니라 부품이므로 this사용하면 에러발생

--- app/src/main/java/com/start/fragment_20220527/fragments/FirstFragment.kt
+++ app/src/main/java/com/start/fragment_20220527/fragments/FirstFragment.kt
@@ -1,20 +1,39 @@
 package com.start.fragment_20220527.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.start.fragment_20220527.R
+import com.start.fragment_20220527.databinding.FragmentFirstBinding
 
 class FirstFragment: Fragment() {
+
+    lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate( R.layout.fragment_first, container, false )
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_first,container,false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnLog.setOnClickListener {
+           Log.d("첫번째프래그먼트","테스트로그")
+        }
+
+        binding.btnLog.setOnClickListener {
+            Toast.makeText(requireContext(), "프래그먼트에서 토스트띄우기", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
