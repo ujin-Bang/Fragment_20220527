29d5f2f49b6aa51ed64a3f5e27f3b2088257eedf
ujin-Bang <qkddnwls14@gmail.com>
2022-05-27 10:48:55 +0900

    Initial commit

---
+++ app/src/main/java/com/start/fragment_20220527/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.start.fragment_20220527
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.start.fragment_20220527.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
